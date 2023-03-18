ca31eb819a8a16c4eb6d39003e7544256a8b3289
Richard SARTORI <richard.sartori223@orange.fr>
2023-03-18 01:53:06 +0100

    complete inria cluster profile

--- bashrc/hosts/cluster_inria.sh
+++ bashrc/hosts/cluster_inria.sh
@@ -1,13 +1,16 @@
 case $HOSTNAME in
 
 	devel*)
-		DEFAULT_PART="routage -w miriel042"
+		DEFAULT_PART="routage -C miriel"
 		DEFAULT_TIME="12:00:00"
 		;;
 
 	miriel*)
 		WORKDIR=/home/rsartori/THESE
-		module load compiler/gcc/11.2.0 hardware/hwloc/2.7.0 build/cmake mpi/openmpi/4.0.3
+		STOREDIR=/beegfs/rsartori
+		alias cds="cd $STOREDIR"
+		export LLVM_HOME=${STOREDIR}/INSTALL
+		module load compiler/gcc hardware/hwloc build/cmake mpi/openmpi
 		;;
 
 esac
