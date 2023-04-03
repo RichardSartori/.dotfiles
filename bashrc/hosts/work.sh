ca4baae31c0eb10c08d1fb512b9ef3324b681e78
SARTORI, Richard <richard.sartori@atos.net>
2023-04-03 17:29:17 +0200

    minor changes to work profile

--- bashrc/hosts/work.sh
+++ bashrc/hosts/work.sh
@@ -12,6 +12,8 @@ export MPI_HOME=${LIBS_DIR}/openmpi-4.1.4/install
 export DUMPI_HOME=${LIBS_DIR}/sst-dumpi/install
 export PGMATH_HOME=${LIBS_DIR}/libpgmath/install
 export RUST_HOME=${LIBS_DIR}/rust
+export RUSTUP_HOME=${RUST_HOME}
+export CARGO_HOME=${RUST_HOME}
 function winopen {
 	path=`realpath $1`
 	rootpath=C:/Users/a800323/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/
