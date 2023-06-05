9a0a2a180165280fbf2c24a55b4b9bf0ffe7937b
SARTORI, Richard <richard.sartori@atos.net>
2023-06-05 11:09:47 +0200

    update cluster_inria.sh

--- bashrc/hosts/cluster_inria.sh
+++ bashrc/hosts/cluster_inria.sh
@@ -1,15 +1,16 @@
+WORKDIR=/home/rsartori/THESE
+STOREDIR=/beegfs/rsartori
+alias cds="cd $STOREDIR"
+
 case $HOSTNAME in
 
 	devel*)
-		DEFAULT_PART="routage -C miriel"
+		DEFAULT_PART="routage -C bora"
 		DEFAULT_TIME="12:00:00"
 		;;
 
-	miriel*)
-		WORKDIR=/home/rsartori/THESE
-		STOREDIR=/beegfs/rsartori
-		alias cds="cd $STOREDIR"
-		export LLVM_HOME=${STOREDIR}/INSTALL
+	miriel* | bora*)
+		export LLVM_HOME=${STOREDIR}/LLVM-on-PlaFRIM/install-15.0.1
 		module load compiler/gcc hardware/hwloc build/cmake mpi/openmpi
 		;;
 
