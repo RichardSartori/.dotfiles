7a223e4fa89410213b09201d99ff45e1a978bf03
Richard SARTORI <richard.sartori223@orange.fr>
2024-01-11 17:03:53 +0100

    update home and work profiles

--- bashrc/hosts/work.sh
+++ bashrc/hosts/work.sh
@@ -1,11 +1,11 @@
-export PROXY=host.containers.internal:9000
+#export PROXY=host.containers.internal:9000
 WORKDIR=~/THESIS-gitlab/llvm_pass
 SHARED_FOLDER=/mnt/c/SHARED_FOLDER
 alias cds="cd $SHARED_FOLDER"
 export LIBS_DIR=/home/rsartori/LIBS
-export LLVM_HOME=${LIBS_DIR}/llvm-16/install
-export MPI_HOME=${LIBS_DIR}/openmpi-4.1.5/install
-export CMAKE_HOME=${LIBS_DIR}/cmake-3.26.4/install
+export LLVM_HOME=${LIBS_DIR}/llvm/install
+export MPI_HOME=${LIBS_DIR}/openmpi/install
+export CMAKE_HOME=${LIBS_DIR}/cmake/install
 export RUSTUP_HOME=${LIBS_DIR}/rust/rustup
 export CARGO_HOME=${LIBS_DIR}/rust/cargo
 alias evince="wslview"
