cf22f7568fac17e2ca4382380b750175970d2d80
SARTORI, Richard <richard.sartori@inria.fr>
2024-10-08 11:44:26 +0200

    update cluster profile

--- hosts/cluster_inria.sh
+++ hosts/cluster_inria.sh
@@ -2,7 +2,8 @@ WORKDIR=/home/rsartori/THESE
 STOREDIR=/beegfs/rsartori
 alias cds="cd $STOREDIR"
 export LIBS_DIR=${STOREDIR}/LIBS
-source /etc/bash_completion.d/git
+module load tools/git
+#source /etc/bash_completion.d/git
 
 case $HOSTNAME in
 
@@ -12,9 +13,11 @@ case $HOSTNAME in
 		;;
 
 	miriel* | bora*)
-		export LLVM_HOME=${LIBS_DIR}/llvm-15/install
-		module load compiler/gcc/12.2.0 mpi/openmpi/4.1.5
-		module load hardware/hwloc build/cmake
+		export LLVM_HOME=${LIBS_DIR}/llvm/install
+		export MPI_HOME=${LIBS_DIR}/mpi/install
+		export CMAKE_HOME=${LIBS_DIR}/cmake/install
+		module load compiler/gcc/12.2.0 hardware/hwloc
+		#module load build/cmake mpi/openmpi/4.1.5
 		;;
 
 esac
